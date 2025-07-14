81c85f403118409aac61ad08494f4a19e9636853
Samuel Quaresma <squoliver@gmail.com>
2025-07-14 12:58:16 -0300

    Serach bar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,7 +1,21 @@
+import './styles.css';
 import Header from "../../components/Header";
+import SearchBar from '../../components/SearchBar';
 
 export default function Catalog() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main className="ct-catalog-main">
+                <section id="ct-catalog-section" className="ct-container">
+                    <div className="ct-search-bar-container">
+                        <SearchBar />
+                    </div>
+                    <div className="ct-catalog-cards-container">
+                        <div>Teste</div>
+                    </div>
+                </section>
+            </main>
+        </>
     );
 }
